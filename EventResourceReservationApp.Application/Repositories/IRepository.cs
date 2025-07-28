a974e1d6bbb046d516853c60d3f76da1fee0384a
jose Angel <josegallardo.cor@gmail.com>
2025-07-28 15:27:27 -0600

    feat: interfaz generica repository

---
+++ EventResourceReservationApp.Application/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventResourceReservationApp.Application.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task AddAsync(T entity);
+        Task RemoveASync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            string? includeProperties = null);
+        Task<T?> GetFirstOrDefaultAsync(Expression<Func<T, bool>> filter,
+            string? includeProperties = null);
+    }
+}
