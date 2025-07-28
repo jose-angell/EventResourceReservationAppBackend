8d0189c29075b562112c78be5ccf24b58b45ac2c
jose Angel <josegallardo.cor@gmail.com>
2025-07-28 15:46:52 -0600

    feat: interfaz para repositorio de categoria

---
+++ EventResourceReservationApp.Application/Repositories/ICategoriaRepository.cs
@@ -0,0 +1,15 @@
+﻿using EventResourceReservationApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventResourceReservationApp.Application.Repositories
+{
+    public interface ICategoriaRepository: IRepository<Category>
+    {
+        Task<Category> GetByIdAsync(int id);
+        Task UpdateAsync(Category category);
+    }
+}
