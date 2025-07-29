fc1ed8950d85fd6f29772ae8b601e0d45912849e
jose Angel <josegallardo.cor@gmail.com>
2025-07-28 19:26:25 -0600

    feat: implementacion de CategoryRepository

---
+++ EventResourceReservationApp.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using EventResourceReservationApp.Application.Repositories;
+using EventResourceReservationApp.Domain;
+using EventResourceReservationApp.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventResourceReservationApp.Infrastructure.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+        public async Task<Category> GetByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await Task.CompletedTask;
+        }
+    }
+}
