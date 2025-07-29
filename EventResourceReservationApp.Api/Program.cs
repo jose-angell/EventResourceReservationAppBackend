a4911eeb65c33562abe2e5d9af15217c3f40c0bc
jose Angel <josegallardo.cor@gmail.com>
2025-07-28 19:37:44 -0600

    feat: implementacion del patron unidad de trabajo y se injecto la dependencia

--- EventResourceReservationApp.Api/Program.cs
+++ EventResourceReservationApp.Api/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using EventResourceReservationApp.Infrastructure.Data;
+using EventResourceReservationApp.Application.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUnitOfWork, IUnitOfWork>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ EventResourceReservationApp.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using EventResourceReservationApp.Application.Repositories;
+using EventResourceReservationApp.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventResourceReservationApp.Infrastructure.Repositories
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public ICategoryRepository Categories { get; private set; }
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            Categories = new CategoryRepository(_context);
+        }
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this); // le indica a Garbage Collector que no necesita llamar al finalizador de esta clase
+        }
+    }
+}
