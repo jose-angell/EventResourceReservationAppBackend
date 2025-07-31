d64744eecc76c1adaec60a266e2dfd5cbc5c59f6
jose Angel <josegallardo.cor@gmail.com>
2025-07-30 18:48:27 -0600

    feat: dfinicion de clase compartida para manejar excepciones de la capa de infra en el usecase

---
+++ EventResourceReservationApp.Application/Common/PersistenceException.cs
@@ -0,0 +1,9 @@
+﻿namespace EventResourceReservationApp.Application.Common
+{
+    public class PersistenceException : Exception
+    {
+        public PersistenceException() { }
+        public PersistenceException(string massage) : base(massage) { }
+        public PersistenceException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
