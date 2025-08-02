940c7c676c005cfc5d457041770db0c8e367c05c
jose Angel <josegallardo.cor@gmail.com>
2025-08-01 21:35:00 -0600

    feat: creacion de endpoint para la actualizacion de la categoria

--- EventResourceReservationApp.Api/Controllers/CategoriesController.cs
+++ EventResourceReservationApp.Api/Controllers/CategoriesController.cs
@@ -9,11 +9,14 @@ namespace EventResourceReservationApp.Api.Controllers
     public class CategoriesController : ControllerBase
     {
         private readonly CreateCategoryUseCase _createCategoryUseCase;
+        private readonly UpdateCategoryUseCase _UpdateCategoryUseCase;
         public CategoriesController(
-            CreateCategoryUseCase createCategoryUseCase
+            CreateCategoryUseCase createCategoryUseCase,
+            UpdateCategoryUseCase updateCategoryUseCase
             )
         {
             _createCategoryUseCase = createCategoryUseCase;
+            _UpdateCategoryUseCase = updateCategoryUseCase;
         }
         [HttpPost]
         public async Task<IActionResult> CreateCategory([FromBody] CreateCategoryRequest request)
@@ -29,6 +32,31 @@ namespace EventResourceReservationApp.Api.Controllers
                 {
                     Message = result.Message,
                     Category = result.Data
+                });
+            }
+            else
+            {
+                return BadRequest(new ProblemDetails
+                {
+                    Status = StatusCodes.Status400BadRequest,
+                    Title = "Error en la operación",
+                    Detail = result.Message
+                });
+            }
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory([FromBody] UpdateCategoryRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _UpdateCategoryUseCase.ExecuteAsync(request);
+            if (result.IsSuccess)
+            {
+                return Ok(new
+                {
+                    Message = result.Message
                 });
             }
             else
