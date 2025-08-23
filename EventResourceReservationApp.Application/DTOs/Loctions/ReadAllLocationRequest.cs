c494261d002790e54f66ee1291b266d48da228a2
jose Angel <josegallardo.cor@gmail.com>
2025-08-22 23:17:35 -0600

    feat: ajustes de los deteo para la lectura y acutalizacion de ubicaicones

---
+++ EventResourceReservationApp.Application/DTOs/Loctions/ReadAllLocationRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventResourceReservationApp.Application.DTOs.Loctions
+{
+    public class ReadAllLocationRequest
+    {
+        public string? City { get; set; }
+        public int? ZipCode { get; set; }
+        public Guid? CreatedByUserIdFilter { get; set; }
+        public string? OrderBy { get; set; }
+    }
+}
