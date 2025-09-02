af599bb3288756add76356602a0e3f17d96c14d2
jose Angel <josegallardo.cor@gmail.com>
2025-09-01 22:00:15 -0600

    feat: creacion de los Dtos para el manejo de los items en el carrito de compras

---
+++ EventResourceReservationApp.Application/DTOs/ReservationCarItems/CreateReservationCarItemRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventResourceReservationApp.Application.DTOs.ReservationCarItems
+{
+    public class CreateReservationCarItemRequest
+    {
+        [Required(ErrorMessage = "El Id del cliente es requerido.")]
+        public Guid ClientId { get; set; }
+
+        [Required(ErrorMessage = "El Id del recurso es requerido.")]
+        public Guid ResourceId { get; set; }
+
+        [Required(ErrorMessage = "La cantidad es requerida.")]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor que cero.")]
+        public int Quantity { get; set; }
+    }
+}
---
+++ EventResourceReservationApp.Application/DTOs/ReservationCarItems/UpdateReservationCarItemRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventResourceReservationApp.Application.DTOs.ReservationCarItems
+{
+    public class UpdateReservationCarItemRequest
+    {
+        [Required(ErrorMessage = "El Id del item del carrito es requerido.")]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "La cantidad es requerida.")]
+        public int Quantity { get; set; }
+    }
+}
