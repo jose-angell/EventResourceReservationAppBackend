91645d77dc444d3f2653869766ed2da98e0eac25
jose Angel <josegallardo.cor@gmail.com>
2025-09-01 22:15:07 -0600

    feat: creacion de interfaz para ResevationCarItem

---
+++ EventResourceReservationApp.Application/Repositories/IReservationCarItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using EventResourceReservationApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventResourceReservationApp.Application.Repositories
+{
+    public interface IReservationCarItemRepository: IRepository<ReservationCarItem>
+    {
+        Task<ReservationCarItem> GetByIdAsync(Guid id);
+        Task UpdateAsync(ReservationCarItem item);
+    }
+}
