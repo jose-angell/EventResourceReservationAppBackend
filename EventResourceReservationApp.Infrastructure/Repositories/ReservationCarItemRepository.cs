97bf1e09501b327d7e3b2d21d9a8bd4d91b3e231
jose Angel <josegallardo.cor@gmail.com>
2025-09-01 22:29:42 -0600

    feat: creacion de la implementacion del respostorio para el Resevation Car Item

---
+++ EventResourceReservationApp.Infrastructure/Repositories/ReservationCarItemRepository.cs
@@ -0,0 +1,29 @@
+﻿using EventResourceReservationApp.Application.Repositories;
+using EventResourceReservationApp.Domain;
+using EventResourceReservationApp.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventResourceReservationApp.Infrastructure.Repositories
+{
+    public class ReservationCarItemRepository: Repository<ReservationCarItem>, IReservationCarItemRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ReservationCarItemRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+        public async Task<ReservationCarItem> GetByIdAsync(Guid id)
+        {
+            return await _context.ReservationCarItems.FindAsync(id);
+        }
+        public async Task UpdateAsync(ReservationCarItem item)
+        {
+            _context.ReservationCarItems.Update(item);
+            await Task.CompletedTask;
+        }
+    }
+}
