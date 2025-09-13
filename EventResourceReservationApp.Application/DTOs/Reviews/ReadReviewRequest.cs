b31c41ff284394b5207a30c1c197006a93b1194e
jose Angel <josegallardo.cor@gmail.com>
2025-09-13 01:31:56 -0600

    feat: creacion de los DTOs para el manejo de los modelos

---
+++ EventResourceReservationApp.Application/DTOs/Reviews/ReadReviewRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventResourceReservationApp.Application.DTOs.Reviews
+{
+    public class ReadReviewRequest
+    {
+        public Guid ResourceId { get; set; }
+        public Guid UserId { get; set; }
+        public int Rating { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
