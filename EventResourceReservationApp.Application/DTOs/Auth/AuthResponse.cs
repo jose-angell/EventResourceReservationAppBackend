aa588353ae4b058e99c5716dd59543687098526e
jose Angel <josegallardo.cor@gmail.com>
2025-10-17 23:20:20 -0600

    feat: se agregaron los dtos para la autenticacion y registros

---
+++ EventResourceReservationApp.Application/DTOs/Auth/AuthResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventResourceReservationApp.Application.DTOs.Auth
+{
+    public class AuthResponse
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public IList<string> Roles { get; set; } = new List<string>();
+    }
+}
