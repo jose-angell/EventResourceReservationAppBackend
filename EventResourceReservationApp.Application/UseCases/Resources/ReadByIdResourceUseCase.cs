d48b1ed19ca0db717bd7abfb3a7d70830afcc9d1
jose Angel <josegallardo.cor@gmail.com>
2025-12-05 18:01:15 -0600

    feat: creacion de caso de uso para consultar un recursos solo usando el id

---
+++ EventResourceReservationApp.Application/UseCases/Resources/ReadByIdResourceUseCase.cs
@@ -0,0 +1,78 @@
+﻿using EventResourceReservationApp.Application.Common;
+using EventResourceReservationApp.Application.DTOs.Resources;
+using EventResourceReservationApp.Application.Repositories;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventResourceReservationApp.Application.UseCases.Resources
+{
+    public class ReadByIdResourceUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<ReadByIdResourceUseCase> _logger;
+        public ReadByIdResourceUseCase(IUnitOfWork unitOfWork, ILogger<ReadByIdResourceUseCase> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+        public async Task<OperationResult<ResourceResponse>> ExecuteAsync(Guid resourceId)
+        {
+            try
+            {
+                var resource = await _unitOfWork.Resources.GetByIdAsync(resourceId);
+                if (resource == null)
+                {
+                    _logger.LogWarning($"Recurso con Id '{resourceId}' no encontrado.");
+                    return OperationResult<ResourceResponse>.Failure(
+                     $"No se enconto el recurso con Id '{resourceId}'",
+                    "NotFound",
+                    $"La operación de busqueda falló debido a que no se encontró el recurso con Id '{resourceId}'."
+                    );
+                }
+                // TODO: el calculo de totales en uso solo se hace en GetByIdAndDateRangeAsync, revisar si es necesario implementarlo aqui tambien
+
+
+                var response = new ResourceResponse
+                {
+                    Id = resource.Id,
+                    StatusId = resource.StatusId,
+                    StatusDescription = "",
+                    Name = resource.Name,
+                    Description = resource.Description,
+                    Price = resource.Price,
+                    Quantity = resource.Quantity,
+                    QuantityInUse = resource.Quantity, //resource.QuantityInUse,
+                    CategoryId = resource.CategoryId,
+                    CategoryName = resource.Category?.Name ?? "",
+                    LocationId = resource.LocationId,
+                    LocationDescription = resource.Location?.City ?? "",
+                    AuthorizationType = resource.AuthorizationType,
+                    Created = resource.CreatedAt
+                };
+                return OperationResult<ResourceResponse>.Success(response, "Recurso encontrado exitosamente.");
+            }
+            catch (PersistenceException pEx)
+            {
+                _logger.LogError(pEx, $"Fallo al obtener recurso con id '{resourceId}' debido a un error de persistencia.");
+                return OperationResult<ResourceResponse>.Failure(
+                    "No se pudo obtener el recurso de la base de datos.",
+                    "PersistenceError",
+                    $"La operación de lectura falló debido a un problema de almacenamiento de datos para el recurso con Id '{resourceId}'."
+                );
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Ocurrió un error inesperado durante la obtención del recurso con Id '{resourceId}' en el caso de uso.");
+                return OperationResult<ResourceResponse>.Failure(
+                    "Ocurrió un error interno imprevisto.",
+                    "UnexpectedError",
+                    $"La operación de lectura falló debido a un problema inesperado para el recurso con Id '{resourceId}'."
+                );
+            }
+        }
+    }
+}
--- EventResourceReservationApp.Infrastructure/Repositories/ResourceRepository.cs
+++ EventResourceReservationApp.Infrastructure/Repositories/ResourceRepository.cs
@@ -1,24 +1,30 @@
 ﻿using EventResourceReservationApp.Application.Repositories;
 using EventResourceReservationApp.Domain;
 using EventResourceReservationApp.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace EventResourceReservationApp.Infrastructure.Repositories
 {
     public class ResourceRepository: Repository<Resource>, IResourceRepository
     {
         private readonly ApplicationDbContext _context;
-        public ResourceRepository(ApplicationDbContext context): base(context)
+        public ResourceRepository(ApplicationDbContext context) : base(context)
         {
             _context = context;
         }
-        public async Task<Resource> GetByIdAsync(Guid id)
+        public async Task<Resource?> GetByIdAsync(Guid id)
         {
-            return await _context.Resources.FindAsync(id);
+            return await _dbSet
+                .Include(r => r.CreatedByUser) // Incluir el usuario que creó el recurso
+                .Include(r => r.Location) // Incluir la ubicación del recurso
+                .Include(r => r.Category) // Incluir la categoría del recurso
+                .FirstOrDefaultAsync(r => r.Id == id); // Filtrar por Id
         }
         public async Task UpdateAsync(Resource resource)
         {
