563f0d7227cf538ff4ebfcce1c284321de5fa54f
jose Angel <josegallardo.cor@gmail.com>
2025-12-14 23:18:43 -0600

    feat: creacion de repositorios para reservas

---
+++ EventResourceReservationApp.Application/Repositories/IReservationRepository.cs
@@ -0,0 +1,15 @@
+﻿using EventResourceReservationApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventResourceReservationApp.Application.Repositories
+{
+    public interface IReservationRepository
+    {
+        Task<Reservation> GetByIdAsync(Guid id);
+        Task Update(Reservation reservation);
+    }
+}
---
+++ EventResourceReservationApp.Infrastructure/Repositories/ReservationRepository.cs
@@ -0,0 +1,30 @@
+﻿using EventResourceReservationApp.Application.Repositories;
+using EventResourceReservationApp.Domain;
+using EventResourceReservationApp.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventResourceReservationApp.Infrastructure.Repositories
+{
+    public class ReservationRepository : Repository<Reservation>, IReservationRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ReservationRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+        public async Task<Reservation> GetByIdAsync(Guid id)
+        {
+            return await _context.Reservations.FindAsync(id); // Filtrar por Id
+        }
+        public async Task Update(Reservation reservation)
+        {
+            _context.Reservations.Update(reservation);
+            await Task.CompletedTask;
+        }
+    }
+}
