0220baf5a36d78f55f1bff2a1687b0668c0b53dd
jose Angel <josegallardo.cor@gmail.com>
2025-12-25 22:08:03 -0600

    feat: cracion de migracion de reservationdetail

---
+++ EventResourceReservationApp.Infrastructure/Migrations/20251226040711_25122025_InitialReservationDetail.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventResourceReservationApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class _25122025_InitialReservationDetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "TransactionId",
+                table: "Reservations",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.CreateTable(
+                name: "ReservationDetails",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ReservationId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ReservationId1 = table.Column<Guid>(type: "uuid", nullable: true),
+                    ResourceId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ResourceId1 = table.Column<Guid>(type: "uuid", nullable: true),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReservationDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReservationDetails_Reservations_ReservationId",
+                        column: x => x.ReservationId,
+                        principalTable: "Reservations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ReservationDetails_Reservations_ReservationId1",
+                        column: x => x.ReservationId1,
+                        principalTable: "Reservations",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ReservationDetails_Resources_ResourceId",
+                        column: x => x.ResourceId,
+                        principalTable: "Resources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ReservationDetails_Resources_ResourceId1",
+                        column: x => x.ResourceId1,
+                        principalTable: "Resources",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReservationDetails_ReservationId",
+                table: "ReservationDetails",
+                column: "ReservationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReservationDetails_ReservationId1",
+                table: "ReservationDetails",
+                column: "ReservationId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReservationDetails_ResourceId",
+                table: "ReservationDetails",
+                column: "ResourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReservationDetails_ResourceId1",
+                table: "ReservationDetails",
+                column: "ResourceId1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReservationDetails");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "TransactionId",
+                table: "Reservations",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+        }
+    }
+}
