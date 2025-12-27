046a8733cee325f941f9faec8b61b08dbcb327a9
jose Angel <josegallardo.cor@gmail.com>
2025-12-27 00:59:25 -0600

    FEAT:  creacion de DTOs para reservation details

---
+++ EventResourceReservationApp.Application/DTOs/ReservationDetails/UpdateReservationDetailRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventResourceReservationApp.Application.DTOs.ReservationDetails
+{
+    public class UpdateReservationDetailRequest
+    {
+        public Guid Id { get; set; }
+        public int Quantity { get; set; }
+    }
+}
